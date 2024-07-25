ea80e50f50637d3efdcd5b4dbc73177b5322bf8d
woong-9yo <dndry1072@naver.com>
2024-07-25 16:15:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1720 ms, Memory: 316696 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,61 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+
+            if (m == 0) {
+                System.out.println(n);
+                return;
+            }
+
+            int[][] graph = new int[n][n];
+            boolean[] visited = new boolean[n];
+            Set<Integer> nodeSet = new HashSet<>();
+
+            for (int i = 0; i < m; i++) {
+                int u = scanner.nextInt() - 1;
+                int v = scanner.nextInt() - 1;
+
+                graph[u][v] = 1;
+                graph[v][u] = 1;
+
+                nodeSet.add(u);
+                nodeSet.add(v);
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (!nodeSet.contains(i)) {
+                    graph[i][i] = 1;
+                }
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (!visited[j] && graph[i][j] == 1) {
+                        dfs(graph, visited, j);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    private static void dfs(int[][] graph, boolean[] visited, int n) {
+        visited[n] = true;
+
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[n][i] == 1 && !visited[i]) {
+                dfs(graph, visited, i);
+            }
+        }
+    }
+}
