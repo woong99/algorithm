58ec0a3b4f3c40bdd00590b80f0cf251f126e6c4
woong-9yo <dndry1072@naver.com>
2024-07-25 13:26:51 +0900

    [Silver I] Title: 단지번호붙이기, Time: 224 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,73 +1,61 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final int[] dx = {1, -1, 0, 0};
+
+    private static final int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int mapSize = scanner.nextInt(); // 지도 크기
 
             int[][] map = new int[mapSize][mapSize]; // 지도 선언
-            int[][] visited = new int[mapSize][mapSize]; // 방문 여부
+            boolean[][] visited = new boolean[mapSize][mapSize]; // 방문 여부
 
             for (int i = 0; i < mapSize; i++) {
                 String input = scanner.next();
                 for (int j = 0; j < mapSize; j++) {
-                    char c = input.toCharArray()[j];
-                    map[i][j] = Character.getNumericValue(c);
+                    map[i][j] = Character.getNumericValue(input.charAt(j));
                 }
             }
 
             int homeCount = 0;
+            List<Integer> homeSizes = new ArrayList<>();
 
             for (int i = 0; i < mapSize; i++) {
                 for (int j = 0; j < mapSize; j++) {
-                    if (visited[i][j] == 0 && map[i][j] == 1) {
-                        dfs(map, visited, j, i, ++homeCount);
-                    }
-                }
-            }
-
-            Map<Integer, Integer> homeMap = new HashMap<>();
-            for (int i = 0; i < mapSize; i++) {
-                for (int j = 0; j < mapSize; j++) {
-                    if (visited[i][j] != 0) {
-                        homeMap.put(visited[i][j], homeMap.getOrDefault(visited[i][j], 0) + 1);
+                    if (!visited[i][j] && map[i][j] == 1) {
+                        int size = dfs(map, visited, j, i);
+                        homeSizes.add(size);
+                        homeCount++;
                     }
                 }
             }
 
             System.out.println(homeCount);
-            List<Integer> list = new ArrayList<>(homeMap.values());
-            Collections.sort(list);
-
-            for (Integer i : list) {
-                System.out.println(i);
+            Collections.sort(homeSizes);
+            for (int size : homeSizes) {
+                System.out.println(size);
             }
         }
     }
 
-    private static void dfs(int[][] map, int[][] visited, int x, int y, int num) {
-        visited[y][x] = num;
+    private static int dfs(int[][] map, boolean[][] visited, int x, int y) {
+        int count = 1;
+        visited[y][x] = true;
 
-        if (x + 1 < map.length && map[y][x + 1] == 1 && visited[y][x + 1] == 0) { // 오른쪽
-            dfs(map, visited, x + 1, y, num);
-        }
+        for (int direction = 0; direction < 4; direction++) {
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
 
-        if (x - 1 >= 0 && map[y][x - 1] == 1 && visited[y][x - 1] == 0) { // 왼쪽
-            dfs(map, visited, x - 1, y, num);
+            if (nx >= 0 && ny >= 0 && nx < map.length && ny < map.length && map[ny][nx] == 1 && !visited[ny][nx]) {
+                count += dfs(map, visited, nx, ny);
+            }
         }
-
-        if (y + 1 < map.length && map[y + 1][x] == 1 && visited[y + 1][x] == 0) { // 아래쪽
-            dfs(map, visited, x, y + 1, num);
-        }
-
-        if (y - 1 >= 0 && map[y - 1][x] == 1 && visited[y - 1][x] == 0) { // 위쪽
-            dfs(map, visited, x, y - 1, num);
-        }
+        return count;
     }
 }
