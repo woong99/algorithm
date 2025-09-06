b4ca17e3c7e296dd94826e705a0bbf98e23f96f9
JeongWoongGyo <dndry1072@naver.com>
2025-09-06 18:05:36 +0900

    [Silver IV] Title: 수들의 합 2, Time: 184 ms, Memory: 24168 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.kt
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.kt
@@ -3,21 +3,16 @@ fun main() {
     val arr = readln().split(" ").map { it.toInt() }
 
     var count = 0
-    for (i in arr.indices) {
-        var pointer = i
-        var sum = 0
-        while (true) {
-            sum += arr[pointer]
-            pointer += 1
-
-            if (sum == m) {
-                count++
-                break
-            } else if (sum > m) {
-                break
-            }
-            if (pointer >= arr.size) break
+    var left = 0
+    var sum = 0
+    for (right in arr.indices) {
+        sum += arr[right]
+        while (sum > m && right > left) {
+            sum -= arr[left]
+            left++
         }
+        
+        if (sum == m) count++
     }
 
     println(count)
